539bea61881d0a7fc1a0288df2f37276f235bad8
TESRobotics539 <trinityrobotics@trinityes.org>
2026-02-23 16:21:30 -0500

    Updated config

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -90,7 +90,7 @@ public class Swerve extends SubsystemBase
     swerveDrive.setAngularVelocityCompensation(true,
                                                true,
                                                0.1); //Correct for skew that gets worse as angular velocity increases. Start with a coefficient of 0.1.
-    swerveDrive.setModuleEncoderAutoSynchronize(true, // TODO: enabled this
+    swerveDrive.setModuleEncoderAutoSynchronize(false, 
                                                 1); // Enable if you want to resynchronize your absolute encoders and motor encoders periodically when they are not moving.
     // swerveDrive.pushOffsetsToEncoders(); // Set the absolute encoder to be used over the internal encoder and push the offsets onto it. Throws warning if not possible
   
